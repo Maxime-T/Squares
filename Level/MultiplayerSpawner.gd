0bfc59455e0aa55c22d8b24f360e16857e06fef7
Adrian Morel <adriandi@hotmail.ch>
2024-07-18 22:46:47 +0200

    TileSet et TileMap

--- Level/MultiplayerSpawner.gd
+++ Level/MultiplayerSpawner.gd
@@ -1,6 +1,9 @@
 extends MultiplayerSpawner
 
 @export var playerScene : PackedScene
+@export var SpawnPos1 : Marker2D
+@export var SpawnPos2 : Marker2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_function = spawnPlayer
@@ -12,9 +15,13 @@ func _ready():
 var players = {}
 
 func spawnPlayer(data):
-	print(data)
 	var p : Node2D = playerScene.instantiate()
-	p.position = Vector2(randi_range(1, 700),randi_range(1, 700))
+	
+	if players.size() == 0:
+		p.position = SpawnPos1.global_position
+	if players.size() == 1:
+		p.position = SpawnPos2.global_position
+	
 	p.set_multiplayer_authority(data)
 	players[data] = p
 	return p
