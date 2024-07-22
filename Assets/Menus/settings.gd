521e27f95a76c319135474598b71b2ce061f60b0
Adrian Morel <adriandi@hotmail.ch>
2024-07-22 18:12:56 +0200

    Life Bar V2 + Input rebind

--- Assets/Menus/settings.gd
+++ Assets/Menus/settings.gd
@@ -2,7 +2,21 @@ extends Control
 
 @export var VolumeSlider : HSlider
 
+@onready var input_button_scene = preload("res://Assets/Menus/input_button.tscn")
+@export var action_list : VBoxContainer 
+var input_actions = {
+	"Left": "Turn left",
+	"Right": "Turn right",
+	"Up": "Move up",
+	"Down": "Move down"
+}
+
+var isRemapping = false
+var actionToRemap = null
+var remappingButton = null
+
 var Resolutions = [Vector2(960,540), Vector2(1920,1080), Vector2(2560,1080)]
+
 
 func _input(event):
 	if !is_multiplayer_authority():
@@ -17,6 +31,9 @@ func _ready():
 	#Window mode (par def)
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+	
+	#Input settings
+	create_action_list()
 	
 
 func _on_h_slider_value_changed(value):
@@ -52,3 +69,23 @@ func _on_check_full_s_item_selected(index):
 
 func _on_resolutions_item_selected(index):
 	DisplayServer.window_set_size(Resolutions[index])
+
+func create_action_list():
+	InputMap.load_from_project_settings()
+	for item in action_list.get_children():
+		item.queue_free()
+	
+	for action in input_actions:
+		var button = input_button_scene.instantiate()
+		var ActionLabel = button.find_child("LabelAction")
+		var InputLabel = button.find_child("LabelInput")
+		
+		ActionLabel.text = input_actions[action]
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			InputLabel.text = events[0].as_text().trim_suffix(" (Physical)")
+		else:
+			InputLabel.text = ""
+		
+		action_list.add_child(button)
--- Assets/Player/HUD.gd
+++ Assets/Player/HUD.gd
@@ -1,8 +1,13 @@
 extends Control
 
 @export var health_component : Health
-@export var life_bar : ProgressBar
+@export var hp_bar : ProgressBar
+@export var dmg_bar : ProgressBar
 @export var name_label : Label
+@export var dmgTimer : Timer
+
+@export var barSize : int = 100
+@export var barHeight : int = -90
 
 var MAX_HP : float
 
@@ -11,14 +16,31 @@ func _ready():
 		name_label.text = Steam.getPersonaName()
 	
 	MAX_HP = health_component.MAX_HP
-	life_bar.max_value = MAX_HP
-	life_bar.value = MAX_HP
+	hp_bar.max_value = MAX_HP
+	dmg_bar.max_value = MAX_HP
+	dmg_bar.value = MAX_HP
+	hp_bar.value = MAX_HP
 	
+	custom_minimum_size.x = barSize
+	position.y = barHeight
 
+##Attention: il faut connect Health_changed du HealthComponent
 func _on_health_healt_changed(hp):
+	dmgTimer.start()
 	rpc("update_hp", hp)
 
+func adjust_dmg_bar():
+	var trueHP = hp_bar.value
+	while(dmg_bar.value != trueHP):
+		await get_tree().create_timer(0.05).timeout #D'après la doc, 0.05 c'est le min pour un timer
+		if (dmg_bar.value - trueHP >= MAX_HP/50):
+			dmg_bar.value -= MAX_HP/50 # =2 pour les players, c'est juste si un truc a genre 10000hp
+		else:
+			dmg_bar.value = trueHP
 
 @rpc("call_local", "any_peer", "reliable")
 func update_hp(hp):
-	life_bar.value = hp
+	hp_bar.value = hp
+
+func _on_dmg_update_cd_timeout():
+	adjust_dmg_bar()
